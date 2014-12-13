9a25c51d07d03152e30a221d76b72532af4c12c7
Mike Schwörer <mailport@mikescher.de>
2014-12-13 17:49:03 +0100

    Executes subrion.install

---
+++ includes/config.inc.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Subrion Open Source CMS 3.2.3
+ * Config file generated on 13 December 2014 14:02:01
+ */
+
+// DEV CONFIG
+
+define('INTELLI_CONNECT', 'mysqli');
+define('INTELLI_DBHOST', 'localhost');
+define('INTELLI_DBUSER', 'root');
+define('INTELLI_DBPASS', '');
+define('INTELLI_DBNAME', 'samdevdb');
+define('INTELLI_DBPORT', '3306');
+define('INTELLI_DBPREFIX', 'sbr323_');
+
+define('IA_SALT', '#252E2BC234');
+
+// debug mode: 0 - disabled, 1 - enabled
+define('INTELLI_DEBUG', 1);
